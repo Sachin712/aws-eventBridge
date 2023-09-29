c03072ba4fe2a3abb46e4a37e1570e5f698d1bbe
Sachin Devdhar <47501558+Sachin712@users.noreply.github.com>
2023-09-29 14:16:13 -0400

    initial commit

---
+++ bin/main_stack.ts
@@ -0,0 +1,13 @@
+import {Construct} from 'constructs';
+import * as cdk from "aws-cdk-lib";
+
+import { S3Stack } from "../lib/s3_stack";
+
+export class MainStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Create the S3 stack
+    const s3Stack = new S3Stack(this, 'S3Stack');
+  }
+}
---
+++ lib/s3_stack.ts
@@ -0,0 +1,16 @@
+import {Construct} from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as core from 'aws-cdk-lib/core';
+
+export class S3Stack extends core.Stack {
+  public readonly bucket: s3.Bucket;
+
+  constructor(scope: Construct, id: string, props?: core.StackProps) {
+    super(scope, id, props);
+
+    // Create an S3 bucket
+    this.bucket = new s3.Bucket(this, 'MyS3Bucket', {
+      removalPolicy: core.RemovalPolicy.DESTROY, // Only for demo purposes, use proper policies in production
+    });
+  }
+}
