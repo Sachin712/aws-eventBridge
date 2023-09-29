47bfbf131aab4ac54b35f3bfdb3e8b4eb922d91e
Sachin Devdhar <47501558+Sachin712@users.noreply.github.com>
2023-09-29 16:06:43 -0400

    change structure

---
+++ service/s3FileUpload.js
@@ -0,0 +1,26 @@
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+
+import {globals as G} from '../infra/lib/globals';
+
+export async function handler(event) {
+  const params = {
+    Bucket: G.s3_bucketName,
+    Key: "example-file.txt",
+    Body: "Hello, world!",
+  };
+
+  try {
+    await s3.upload(params).promise();
+    return {
+      statusCode: 200,
+      body: "File uploaded successfully!",
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: "Error uploading file",
+    };
+  }
+}
