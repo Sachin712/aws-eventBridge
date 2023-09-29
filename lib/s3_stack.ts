d3de0a7bd0c506176000430f069deb01107f2946
Sachin Devdhar <47501558+Sachin712@users.noreply.github.com>
2023-09-28 22:51:25 -0400

    create s3 bucket

---
+++ lib/s3_stack.ts
@@ -0,0 +1,15 @@
+import * as cdk from "aws-cdk-lib";
+import * as s3 from "@aws-cdk/aws-s3";
+
+export class S3Stack extends cdk.Stack {
+  public readonly bucket: s3.Bucket;
+
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Create an S3 bucket
+    this.bucket = new s3.Bucket(this, "MyBucket", {
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Change this in production
+    });
+  }
+}
