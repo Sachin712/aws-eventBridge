86860d226459b96fddba017e41b328e900c755a2
Sachin Devdhar <47501558+Sachin712@users.noreply.github.com>
2023-09-29 01:05:23 -0400

    package changes

--- lib/s3_stack.ts
+++ lib/s3_stack.ts
@@ -1,10 +1,11 @@
 import * as cdk from "aws-cdk-lib";
+import {Construct} from 'constructs';
 import * as s3 from "@aws-cdk/aws-s3";
 
 export class S3Stack extends cdk.Stack {
   public readonly bucket: s3.Bucket;
 
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // Create an S3 bucket
