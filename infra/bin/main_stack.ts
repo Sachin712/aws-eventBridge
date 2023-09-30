c6271a0daa28b1e45e9edcd13bf5b32a3ea8b6ce
Sachin Devdhar <47501558+Sachin712@users.noreply.github.com>
2023-09-30 16:56:34 -0400

    fixing tsconfiggit

--- infra/bin/main_stack.ts
+++ infra/bin/main_stack.ts
@@ -1,17 +1,15 @@
-import {Construct} from 'constructs';
 import * as cdk from "aws-cdk-lib";
 
-import  {S3Stack}  from "../lib/s3_stack";
-import { FunctionStack } from '../lib/function_stack';
+import { S3Stack } from "../lib/s3_stack";
+import { FunctionStack } from "../lib/function_stack";
 
-export class MainStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+const app = new cdk.App();
 
-    // Create the S3 stack
-    const s3Stack = new S3Stack(this, 'S3Stack');
+export var s3Stack: any;
+export var functionStack: any;
 
-    // Create the Lambda function stack
-    const functionStack = new FunctionStack(this, 'FunctionStack');
-  }
-}
+// Create the S3 stack
+s3Stack = new S3Stack(app, "S3Stack");
+
+// Create the Lambda function stack
+functionStack = new FunctionStack(app, "FunctionStack");
